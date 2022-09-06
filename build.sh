d661200f5a59641fea25d8cbe6b77f00fb404992
Landon Haugh <landon.haugh@nxp.com>
2022-09-06 10:32:27 -0500

    Update scripts to latest

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-set -x
 
-# make sure cache writes have w other access
-umask 0000
+set -x
 
 BUILD=`date +%Y%m%d.%H%M`; start=`date +%s`
 
-mkdir -p build
-cd build
+mkdir -p ~/yocto/build_510
+cd ~/yocto/build_510
 
 repo init -u https://source.codeaurora.org/external/imx/imx-manifest -b imx-linux-hardknott -m imx-5.10.72-2.2.0_desktop.xml
 repo sync
 
-# allow build to not prompt for input
-sed 's/more\ -d/\#more\ -d/' setup-environment > x && mv -f x setup-environment
-
-cd sources
-git clone git@github.com:NXPHoverGames/NavQPlus.git meta-nxp-hovergames -b patches-bbappend
-cd ..
+cd ~/yocto/build_510/sources
+git clone git@github.com:NXPHoverGames/NavQPlus.git -b lf5.10.72_2.2.2-ros2 meta-nxp-hovergames
+cd ~/yocto/build_510
 
 DISTRO=imx-desktop-xwayland MACHINE=imx8mpnavq EULA=yes source ./imx-setup-desktop.sh -b build
 
-bitbake-layers add-layer ../sources/meta-nxp-hovergames
+cd ~/yocto/build_510/build/conf
+echo BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-hovergames\" >> bblayers.conf
+cd ~/yocto/build_510/build
 
 bitbake imx-image-desktop
 
 finish=`date +%s`; echo "### Build Time = `expr \( $finish - $start \) / 60` minutes"
+
+set +x
+
---
+++ build_15.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+BUILD=`date +%Y%m%d.%H%M`; start=`date +%s`
+
+pwd
+mkdir -p ~/yocto/build_515
+cd ~/yocto/build_515
+pwd
+
+repo init -u https://source.codeaurora.org/external/imx/imx-manifest -b imx-linux-kirkstone -m imx-5.15.32-2.0.0_desktop.xml
+repo sync
+
+cd ~/yocto/build_515/sources
+git clone git@github.com:NXPHoverGames/NavQPlus.git -b lf5.15.32_2.0.0-ros2 meta-nxp-hovergames
+cd ~/yocto/build_515
+
+DISTRO=imx-desktop-xwayland MACHINE=imx8mpnavq EULA=yes source ./imx-setup-desktop.sh -b build
+
+cd ~/yocto/build_515/build/conf
+echo BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-hovergames\" >> bblayers.conf
+cd ~/yocto/build_515/build
+
+bitbake imx-image-desktop
+
+finish=`date +%s`; echo "### Build Time = `expr \( $finish - $start \) / 60` minutes"
+
+set +x
